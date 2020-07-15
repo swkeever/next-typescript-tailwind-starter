e95eff81fdcab4f4502315c328ea4cb35af429d3
Sean Keever <swkeever@gmail.com>
2020-07-15 08:55:38 -0700

    updates

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,6 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
   },
   transformIgnorePatterns: [
     '/node_modules/',
