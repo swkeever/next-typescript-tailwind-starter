a9a8b5c4f7e2fa32d0c782ef1147722e56139d45
Sean Keever <swkeever@gmail.com>
2020-07-14 22:20:33 -0700

    postconfig

--- postcss.config.js
+++ postcss.config.js
@@ -5,30 +5,11 @@ const purgecss = [
     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
 ];
-
 module.exports = {
-  plugins:
-    process.env.NODE_ENV === 'production'
-      ? [
-        'postcss-flexbugs-fixes',
-        [
-          'postcss-preset-env',
-          {
-            autoprefixer: {
-              flexbox: 'no-2009',
-            },
-            stage: 3,
-            features: {
-              'custom-properties': false,
-            },
-          },
-        ],
-        'postcss-import',
-        'tailwindcss',
-        'autoprefixer',
-        ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
-      ]
-      : [
-        // No transformations in development
-      ],
+  plugins: [
+    'postcss-import',
+    'tailwindcss',
+    'autoprefixer',
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 };
